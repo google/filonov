2f53a7039d4d1f61c19995cec55d3c665fb1ba24
Andrei Markin <amarkin@google.com>
2024-11-18 18:42:49 +0400

    [tag] Add option for reading tagging results from CSV file
    Change-Id: I6fb4886027ceb98b6a8dadb0a3ea7ac1f8d64131

--- libs/media_tagging/media_tagging/tagging_result.py
+++ libs/media_tagging/media_tagging/tagging_result.py
@@ -16,8 +16,11 @@
 # pylint: disable=C0330, g-bad-import-order, g-multiple-import
 from __future__ import annotations
 
+import dataclasses
+import os
 from typing import Literal
 
+import pandas as pd
 import pydantic
 
 
@@ -69,3 +72,52 @@ class TaggingResult(pydantic.BaseModel):
   content: tuple[Tag, ...] | Description = pydantic.Field(
     description='tags or description in the result'
   )
+
+
+@dataclasses.dataclass
+class TaggingResultsFileInput:
+  """Specifies column names in input file."""
+
+  identifier_name: str
+  tag_name: str
+  score_name: str
+
+
+def from_file(
+  path: os.PathLike[str],
+  file_column_input: TaggingResultsFileInput,
+  media_type: Literal['image', 'video', 'youtube_video'],
+  min_threshold: float = 0.0,
+) -> list[TaggingResult]:
+  """Build tagging results from a file.
+
+  Args:
+    path: Path to files with tags.
+    file_column_input: Identifiers for building tagging results.
+    media_type: Type of media found in a file.
+    min_threshold: Optional threshold for reducing output size.
+
+  Returns:
+    File content converted to Tagging results.
+
+  Raises:
+    ValueError: If file doesn't have all required input columns.
+  """
+  identifier, tag, score = (
+    file_column_input.identifier_name,
+    file_column_input.tag_name,
+    file_column_input.score_name,
+  )
+  data = pd.read_csv(path)
+  if missing_columns := {identifier, tag, score}.difference(set(data.columns)):
+    raise ValueError(f'Missing column(s) in {path}: {missing_columns}')
+  data = data[data[score] > min_threshold]
+  data['tag'] = data.apply(
+    lambda row: Tag(name=row[tag], score=row[score]),
+    axis=1,
+  )
+  grouped = data.groupby(identifier).tag.apply(list).reset_index()
+  return [
+    TaggingResult(identifier=row[identifier], type=media_type, content=row.tag)
+    for _, row in grouped.iterrows()
+  ]
