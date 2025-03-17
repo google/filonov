75a5608e8d4b41fb7c57c891b8f37f046eedf957
Andrei Markin <amarkin@google.com>
2025-03-18 00:17:46 +0400

    [fl] Move media type into FileInputParameters request
    Change-Id: Ic3c735e8bf7e9934f999c354a2702c59cea90e7e

--- libs/filonov/filonov/inputs/file.py
+++ libs/filonov/filonov/inputs/file.py
@@ -30,6 +30,7 @@ class FileInputParameters(interfaces.InputParameters):
   """File specific parameters for generating creative map."""
 
   path: os.PathLike[str] | str
+  media_type: Literal['IMAGE', 'VIDEO', 'YOUTUBE_VIDEO']
   media_identifier: str = 'media_url'
   media_name: str = 'media_name'
   metric_names: Sequence[str] | str = ('clicks', 'impressions')
@@ -53,7 +54,6 @@ class ExtraInfoFetcher(interfaces.BaseMediaInfoFetcher):
   def generate_extra_info(
     self,
     fetching_request: FileInputParameters,
-    media_type: Literal['IMAGE', 'VIDEO', 'YOUTUBE_VIDEO'],
     with_size_base: str | None = None,
   ) -> dict[str, interfaces.MediaInfo]:
     """Extracts data from Ads API and converts to MediaInfo objects."""
@@ -66,7 +66,7 @@ class ExtraInfoFetcher(interfaces.BaseMediaInfoFetcher):
       )
     return interfaces.convert_gaarf_report_to_media_info(
       performance=performance,
-      media_type=media.MediaTypeEnum[media_type.upper()],
+      media_type=media.MediaTypeEnum[fetching_request.media_type.upper()],
       with_size_base=with_size_base,
       metric_columns=fetching_request.metric_names,
     )
