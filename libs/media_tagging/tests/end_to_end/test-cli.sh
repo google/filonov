f27e4edaf2b134cedfbee7b8acbf91d2411ef260
Andrei Markin <amarkin@google.com>
2025-03-24 16:20:03 +0400

    [tag] Update e2e tests
    Change-Id: I9f6d2a644275472d7265d3e6b761516b96d6da96

--- libs/media_tagging/tests/end_to_end/test-cli.sh
+++ libs/media_tagging/tests/end_to_end/test-cli.sh
@@ -3,106 +3,25 @@ if [ -f $SCRIPT_PATH/.env ]; then
   source $SCRIPT_PATH/.env
 fi
 
-run_vision_api_image() {
-  media-tagger \
-    --media-path $IMAGE \
-    --tagger vision-api \
-    --media-type IMAGE \
-    --db-uri $FILONOV_DB_URI
-}
-run_gemini_unstructured_image() {
-  media-tagger \
-    --media-path $IMAGE \
-    --media-type IMAGE \
-    --tagger gemini-image \
-    --writer json --output output_unstructured \
-    --db-uri $FILONOV_DB_URI \
-    --tagger.n_tags=10
+tag() {
+  local tagger=$1
+  local media_type=$2
+  media-tagger tag \
+    $IMAGE \
+    --tagger $1 \
+    --media-type $media_type
 }
 
-run_gemini_structured_image() {
-  media-tagger \
-    --media-path $IMAGE \
-    --media-type IMAGE \
-    --tagger gemini-structured-image \
-    --writer json --output output_structured \
-    --db-uri $FILONOV_DB_URI \
-    --tagger.tags='fitness, dog, yoga'
+describe() {
+  local tagger=$1
+  local media_type=$2
+  media-tagger tag \
+    $IMAGE \
+    --tagger $1 \
+    --media-type $media_type
 }
 
-run_gemini_description_image() {
-  media-tagger \
-    --media-path $IMAGE \
-    --media-type IMAGE \
-    --tagger gemini-description-image \
-    --db-uri $FILONOV_DB_URI \
-    --writer json --output output_description
-}
-
-run_gemini_unstructured_video() {
-  media-tagger \
-    --media-path $VIDEO \
-    --media-type VIDEO \
-    --tagger gemini-video \
-    --writer json --output output_video_unstructured \
-    --db-uri $FILONOV_DB_URI \
-    --tagger.n_tags=10
-}
-
-run_gemini_structured_video() {
-  media-tagger \
-    --media-path $VIDEO \
-    --media-type VIDEO \
-    --tagger gemini-structured-video \
-    --writer json --output output_video_structured \
-    --db-uri $FILONOV_DB_URI \
-    --tagger.tags='fitness, dog, yoga'
-}
-
-run_gemini_description_video() {
-  media-tagger \
-    --media-path $VIDEO \
-    --media-type VIDEO \
-    --tagger gemini-description-video \
-    --db-uri $FILONOV_DB_URI \
-    --writer json --output output_video_description
-    }
-run_gemini_unstructured_youtube_video() {
-  media-tagger \
-    --media-path $YOUTUBE_LINK \
-    --media-type YOUTUBE_VIDEO \
-    --tagger gemini-youtube-video \
-    --writer json --output output_youtube_video_unstructured \
-    --db-uri $FILONOV_DB_URI \
-    --tagger.n_tags=10
-}
-
-run_gemini_structured_youtube_video() {
-  media-tagger \
-    --media-path $YOUTUBE_LINK \
-    --media-type YOUTUBE_VIDEO \
-    --tagger gemini-structured-youtube-video \
-    --writer json --output output_youtube_video_structured \
-    --db-uri $FILONOV_DB_URI \
-    --tagger.tags='fitness, dog, yoga'
-}
-
-run_gemini_description_youtube_video() {
-  media-tagger \
-    --media-path $YOUTUBE_LINK \
-    --media-type YOUTUBE_VIDEO \
-    --tagger gemini-description-youtube-video \
-    --db-uri $FILONOV_DB_URI \
-    --writer json --output output_youtube_video_description
-    }
-
-run_vision_api_image
-run_gemini_unstructured_image
-run_gemini_structured_image
-run_gemini_description_image
-run_gemini_unstructured_video
-run_gemini_structured_video
-run_gemini_description_video
-run_gemini_unstructured_youtube_video
-run_gemini_structured_youtube_video
-run_gemini_description_youtube_video
+for tagger in 'gemini' 'langchain' 'google-cloud'; do
+  tag $tagger IMAGE
+  describe $tagger IMAGE
+done
