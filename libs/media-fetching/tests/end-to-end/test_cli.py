9b0cf0736ba5e77a90a14b36648ebb6b8505a769
Andrei Markin <amarkin@google.com>
2025-07-10 10:05:58 +0400

    [fet] tests: add end-to-end test for CLI
    Change-Id: I48ab7a18e4d0cfe9a4bc91ccac30473fc570bef1

---
+++ libs/media-fetching/tests/end-to-end/test_cli.py
@@ -0,0 +1,75 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=C0330, g-bad-import-order, g-multiple-import, missing-class-docstring, missing-module-docstring, missing-function-docstring
+
+import os
+import pathlib
+import subprocess
+
+import dotenv
+import pytest
+
+dotenv.load_dotenv()
+_SCRIPT_PATH = pathlib.Path(__file__).parent
+
+filonov_performance_file = os.getenv('FILONOV_PERFORMANCE_RESULTS')
+filonov_account = os.getenv('FILONOV_GOOGLEADS_ACCOUNT')
+filonov_youtube_channel = os.getenv('FILONOV_YOUTUBE_CHANNEL_ID')
+
+
+@pytest.mark.tagger
+class TestMediaFetcher:
+  @pytest.mark.googleads
+  class TestGoogleAdsFetcher:
+    @pytest.mark.parametrize(
+      'media_type',
+      ['IMAGE', 'YOUTUBE_VIDEO'],
+    )
+    def test_fetch(self, media_type):
+      command = (
+        f'media-fetcher --source googleads '
+        f'--media-type {media_type} '
+        f'--googleads.account={filonov_account} '
+        '--googleads.campaign_types=pmax '
+        '--extra-info googleads.main_geo '
+        '--writer console '
+      )
+      result = subprocess.run(command, shell=True, check=False)
+      assert result.returncode == 0
+
+  @pytest.mark.youtube
+  class TestYouTubeFetcher:
+    def test_fetch(self):
+      command = (
+        f'media-fetcher --source youtube '
+        f'--youtube.channel={filonov_youtube_channel} '
+        '--writer console '
+      )
+      result = subprocess.run(command, shell=True, check=False)
+      assert result.returncode == 0
+
+  @pytest.mark.file
+  class TestFileFetcher:
+    def test_fetch(self):
+      command = (
+        f'media-fetcher --source file '
+        f'--file.path={filonov_performance_file} '
+        '--file.media_identifier=media_url '
+        '--file.media_name=asset_name '
+        '--file.metrics=clicks '
+        '--writer console '
+      )
+      result = subprocess.run(command, shell=True, check=False)
+      assert result.returncode == 0
