8b05fde10faa5786ce615b64fdcd8fdd28da7c3c
Andrei Markin <amarkin@google.com>
2025-07-18 18:58:26 +0400

    [fl] feat: add sql/bq/file input for filonov streamlit app
    Change-Id: I12e8353a08daa58ee0779e8a8958dde030d0f5d3

--- libs/filonov/filonov/entrypoints/streamlit_app.py
+++ libs/filonov/filonov/entrypoints/streamlit_app.py
@@ -18,7 +18,9 @@
 
 import datetime
 import json
+import tempfile
 
+import media_fetching
 import media_similarity
 import media_tagging
 import smart_open
@@ -46,19 +48,31 @@ settings = FilonovSettings()
 
 
 def streamlit_app():
+  media_to_campaign_mapping = {
+    'IMAGE': ['app', 'demandgen', 'pmax', 'display', 'search'],
+    'TEXT': ['app', 'demandgen', 'pmax', 'display', 'search'],
+    'YOUTUBE_VIDEO': ['app', 'demandgen', 'pmax'],
+  }
+
   st.title('Creative Map Generator')
 
-  source = st.selectbox('Source', ['youtube', 'googleads'])
+  source = st.selectbox(
+    'Source', ['youtube', 'googleads', 'file', 'bq', 'sqldb']
+  )
 
   with st.form('creative_map_form'):
     name = st.text_input('Name', 'creative_map')
     if source == 'googleads':
       tagger_type = st.selectbox('Tagger Type', ['gemini', 'google-cloud'])
-      media_type = st.selectbox('Media Type', ['IMAGE', 'YOUTUBE_VIDEO'])
-      account = st.text_input('Account', '')
-      campaign_type = st.multiselect(
-        'Campaign type', ['app', 'demandgen', 'pmax', 'video', 'display']
+      media_type = st.selectbox(
+        'Media Type', ['IMAGE', 'YOUTUBE_VIDEO', 'TEXT']
       )
+      account = st.text_input('Account', '')
+
+      campaign_type = st.multiselect(
+        'Campaign type', media_to_campaign_mapping.get(media_type)
+      )
+
       col1, col2 = st.columns(2)
       with col1:
         start_date = st.date_input(
@@ -72,12 +86,53 @@ def streamlit_app():
         )
         if end_date < start_date:
           st.error('End date cannot be less than start_date')
+      extra_info = st.multiselect(
+        'Extra info', ['googleads.main_geo', 'tagging.language']
+      )
       input_parameters = {
         'account': account,
         'campaign_types': campaign_type,
         'start_date': start_date.strftime('%Y-%m-%d'),
         'end_date': end_date.strftime('%Y-%m-%d'),
+        'extra_info': extra_info,
       }
+    elif source in ('bq', 'file', 'sqldb'):
+      input_parameters = {}
+      if source == 'bq':
+        table = st.text_input('bq_table', '')
+        input_parameters['table'] = table
+      elif source == 'sqldb':
+        table = st.text_input('sql_table', '')
+        connection_string = st.text_input('connection_string', '')
+        input_parameters['table'] = table
+        input_parameters['connection_string'] = connection_string
+      elif source == 'file':
+        file_data = st.file_uploader(label='Path to file', type='csv')
+        tmp_file_path = None
+        if file_data:
+          with tempfile.NamedTemporaryFile(
+            delete=False, mode='w', suffix='.csv', encoding='utf-8'
+          ) as f:
+            f.write(file_data.getvalue().decode('utf-8'))
+            tmp_file_path = f.name
+        input_parameters['path'] = tmp_file_path
+      tagger_type = st.selectbox('Tagger Type', ['gemini', 'google-cloud'])
+      media_type = st.selectbox(
+        'Media Type', ['IMAGE', 'YOUTUBE_VIDEO', 'TEXT']
+      )
+
+      media_path_column = st.text_input('Media path column name', 'media_url')
+      media_name = st.text_input('Media column name', 'media_name')
+      metrics = st.text_input('Metric columns', 'clicks,impressions')
+      segments = st.text_input('Segment columns', '')
+      input_parameters.update(
+        {
+          'media_identifier': media_path_column,
+          'media_name': media_name,
+          'metrics': metrics,
+          'segments': segments,
+        }
+      )
     else:
       media_type = 'YOUTUBE_VIDEO'
       tagger_type = 'gemini'
@@ -91,11 +146,15 @@ def streamlit_app():
         source=source,
         media_type=media_type,
         tagger=tagger_type,
-        input_parameters=input_parameters,
+        source_parameters=input_parameters,
         output_parameters=filonov.filonov_service.OutputParameters(
           output_name=name
         ),
       )
+      if 'fetching_service' not in st.session_state:
+        st.session_state['fetching_service'] = (
+          media_fetching.MediaFetchingService(source)
+        )
       if 'tagging_service' not in st.session_state:
         st.session_state['tagging_service'] = media_tagging.MediaTaggingService(
           media_tagging.repositories.SqlAlchemyTaggingResultsRepository(
@@ -113,9 +172,10 @@ def streamlit_app():
           )
         )
       generated_map = filonov.FilonovService(
+        st.session_state['fetching_service'],
         st.session_state['tagging_service'],
         st.session_state['similarity_service'],
-      ).generate_creative_map(source, request)
+      ).generate_creative_map(request)
       destination = utils.build_creative_map_destination(
         request.output_parameters.output_name
       )
