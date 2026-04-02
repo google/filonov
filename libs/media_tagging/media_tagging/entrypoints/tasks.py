9837f9a41ce589999adea23ccf502de19d8ea5c3
Andrei Markin <amarkin@google.com>
2026-04-02 16:52:34 +0400

    feat(tag): add celery task queue and operations endpoint
    Change-Id: I7687752ff2a2e77b5009ff6deda30f27d8c56fec

---
+++ libs/media_tagging/media_tagging/entrypoints/tasks.py
@@ -0,0 +1,46 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import celery
+
+from media_tagging import media_tagging_service, repositories
+
+redis_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+media_tagging_db_url = os.getenv('MEDIA_TAGGING_DB_URL')
+celery_app = celery.Celery(
+  'worker',
+  broker=redis_url,
+  backend=redis_url,
+)
+
+
+service = media_tagging_service.MediaTaggingService(
+  repositories.SqlAlchemyTaggingResultsRepository(media_tagging_db_url)
+)
+
+
+@celery_app.task(pydantic=True)
+def tag(
+  request: media_tagging_service.MediaTaggingRequest,
+) -> media_tagging_service.MediaTaggingResponse:
+  return service.tag_media(request)
+
+
+@celery_app.task(pydantic=True)
+def describe(
+  request: media_tagging_service.MediaTaggingRequest,
+) -> media_tagging_service.MediaTaggingResponse:
+  return service.describe_media(request)
