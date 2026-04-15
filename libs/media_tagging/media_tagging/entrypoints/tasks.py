70c8f0757791b5ffbcfd34927864576edb9cc4d1
Andrei Markin <amarkin@google.com>
2026-04-15 12:21:02 +0400

    feat(tag): add instrumentation for celery
    Change-Id: I24bdbd77563eb46cf8310fd7ab5663f1d95fec91

--- libs/media_tagging/media_tagging/entrypoints/tasks.py
+++ libs/media_tagging/media_tagging/entrypoints/tasks.py
@@ -15,8 +15,15 @@
 import os
 
 import celery
+from garf.executors.entrypoints import utils as garf_utils
+from opentelemetry.instrumentation.celery import CeleryInstrumentor
 
 from media_tagging import media_tagging_service, repositories
+from media_tagging.entrypoints.tracer import (
+  initialize_logger,
+  initialize_meter,
+  initialize_tracer,
+)
 
 redis_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 media_tagging_db_url = os.getenv('MEDIA_TAGGING_DB_URL')
@@ -30,6 +37,19 @@ celery_app = celery.Celery(
 service = media_tagging_service.MediaTaggingService(
   repositories.SqlAlchemyTaggingResultsRepository(media_tagging_db_url)
 )
+
+
+@celery.signals.worker_process_init.connect(weak=False)
+def init_celery_telemetry(*args, **kwargs):
+  otel_service_name = 'media-tagging-celery'
+  initialize_tracer(otel_service_name)
+  initialize_meter(otel_service_name)
+
+  logger = garf_utils.init_logging(
+    loglevel='INFO', logger_type='local', name=otel_service_name
+  )
+  logger.addHandler(initialize_logger(otel_service_name))
+  CeleryInstrumentor().instrument()
 
 
 @celery_app.task(pydantic=True)
